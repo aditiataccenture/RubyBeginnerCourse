656278a2c82a71551d956bf746011eba842c6f25
miscaddy55 <>
2023-03-06 21:29:26 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+#puts 'Hello, World!'
+name = "Aditi"
+organisation = "Palladium"
+nameOfTheCourse = "Beginner"
+puts ("Welcome #{name}, to the #{organisation}'s' #{nameOfTheCourse} course")
+#print ("Welcome #{name}, to the #{organisation}'s' #{nameOfTheCourse} course")
+
+
